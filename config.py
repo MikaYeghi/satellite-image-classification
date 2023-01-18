c1590337484a6750e9a8b125bba03d178c2c6ab1
Mikael Yeghiazaryan <mikaegiazaryan2000@gmail.com>
2023-01-18 17:58:35 -0500

    Added non-centered config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 shape_code = "x1.3z1.3"
 
 """Dataset parameters"""
-DATASET_NAMES = ['organic-recolored-camouflage-centered']
+DATASET_NAMES = ['synthetic']
 TRAIN_PATH = []
 TEST_PATH = []
 if "real" in DATASET_NAMES:
@@ -26,6 +26,9 @@ if "stan-with-shadows" in DATASET_NAMES:
 if "stan-without-shadows" in DATASET_NAMES:
     TRAIN_PATH.append("/home/myeghiaz/Storage/shadowless-shadow-dataset/without-shadows/train")
     TEST_PATH.append("/home/myeghiaz/Storage/shadowless-shadow-dataset/without-shadows/test")
+if "non-centered-no-margin" in DATASET_NAMES:
+    TRAIN_PATH.append("/home/myeghiaz/Storage/SatClass-Real-non-centered-0.125m-50px-no-margin/train")
+    TEST_PATH.append("/home/myeghiaz/Storage/SatClass-Real-non-centered-0.125m-50px-no-margin/test")
 if "adversarial-textures" in DATASET_NAMES:
     TRAIN_PATH.append("/home/myeghiaz/Storage/SatClass-Adversarial-Textures-0.125m-50px")
 if "adversarial-background" in DATASET_NAMES:
@@ -71,24 +74,24 @@ if "modified-shapes" in DATASET_NAMES:
 # TRAIN_PATH.append("/home/myeghiaz/Storage/organic-camouflages-dataset-style/Cam-1")
 
 """Training and testing parameters"""
-NUM_GPUS = 1
+NUM_GPUS = 2
 if NUM_GPUS == 1:
     NUM_DATALOADER_WORKERS = 0
 else:
     NUM_DATALOADER_WORKERS = 6
 BATCH_SIZE = 1024
-# MODEL_WEIGHTS = None
-MODEL_WEIGHTS = "saved_models/synthetic-augmented-vgg/model_final.pth"
+MODEL_WEIGHTS = None
+# MODEL_WEIGHTS = "saved_models/synthetic-augmented-vgg/model_final.pth"
 NUM_CLASSES = 1 # number of foreground classes
 LR = 0.000001
-N_EPOCHS = 10
+N_EPOCHS = 5
 TEST_SIZE = 0.2
-VAL_FREQ = 10
+VAL_FREQ = 1
 # OUTPUT_DIR = f"/home/myeghiaz/Storage/modified-shape-datasets/results/{shape_code}"
-OUTPUT_DIR = "output/"
+OUTPUT_DIR = "saved_models/non-centered-no-margin-baseline/"
 RESULTS_DIR = "results/"
 LOG_DIR = os.path.join(OUTPUT_DIR, "logs")
-EVAL_ONLY = True
+EVAL_ONLY = False
 FP_FN_analysis = True
 APPLY_TRAIN_TRANSFORMS = False
 MODEL_NAME = 'vgg16'
@@ -110,8 +113,8 @@ NEGATIVE_LIMIT_TRAIN = 10
 POSITIVE_LIMIT_TEST = 1000
 NEGATIVE_LIMIT_TEST = 1000
 CAMOUFLAGE_TEXTURES_PATH = "/home/myeghiaz/Storage/organic-camouflages-dataset-style"
-DESCRIPTIVE_COLORS_PATH = "/home/myeghiaz/Storage/descriptive-colors-real-train-fraction-0.1"
-DRESS_CAMOUFLAGE = 'organic' # fixed - uses CAMOUFLAGE_TEXTURES_PATH, random - builds random camouflages from DESCRIPTIVE_COLORS_PATH, 'organic' - load organic camouflages and replace their colors with colors from the dataset, None - no camouflage augmentation is applied
+DESCRIPTIVE_COLORS_PATH = "/home/myeghiaz/Storage/descriptive-colors-real-train-fraction-0.1/cluster_centers_5.pth"
+DRESS_CAMOUFLAGE = None # fixed - uses CAMOUFLAGE_TEXTURES_PATH, random - builds random camouflages from DESCRIPTIVE_COLORS_PATH, 'organic' - load organic camouflages and replace their colors with colors from the dataset, None - no camouflage augmentation is applied
 PIXELATION_BLOCK_SIZE = 16
 NUM_DESCRIPTIVE_COLORS = 10
 COLORS_PER_CAMOUFLAGE = 4
@@ -121,6 +124,7 @@ BRIGHTNESS_LEVELS = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
 ATTACK_LR = 0.1
 HEATMAP_NAME = "corr_heatmap"
 VISUALIZE_HEATMAP_SAMPLES = True
-ATTACKED_PARAMS = ['textures']
-ADVERSARIAL_SAVE_DIR = "/home/myeghiaz/Storage/SatClass-Adversarial-0.125m-50px/Textures-EOT-with-differences/3"
+ATTACKED_PARAMS = ['discrete-textures']
+ADVERSARIAL_SAVE_DIR = "/home/myeghiaz/Storage/SatClass-Adversarial-Discrete-Textures-0.125m-50px"
+PIXELATED_TEXTURE_BLOCK_SIZE = 8 # Setting to 1moves from discrete attacks to ordinary texture attacks
 NUM_ADV_IMGS = 1 # Number of adversarial images that will be generated during the attack
